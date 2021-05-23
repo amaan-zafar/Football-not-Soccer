e370160b1c5ccc655706507612b67824c6d33329
Amaan <azafar2000@gmail.com>
2021-05-24 01:36:17 +0530

    created flutter project

---
+++ football_not_soccer/android/app/src/main/kotlin/com/example/football_not_soccer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.football_not_soccer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ football_not_soccer/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
+
