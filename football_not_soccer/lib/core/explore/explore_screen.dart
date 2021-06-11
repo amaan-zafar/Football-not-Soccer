992f5174f5f9da67f645a9249474996c2129f575
Amaan <azafar2000@gmail.com>
2021-06-11 20:59:08 +0530

    commit

--- football_not_soccer/lib/core/explore/explore_screen.dart
+++ football_not_soccer/lib/core/explore/explore_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:football_not_soccer/config/icons/app_icons_icons.dart';
 import 'package:football_not_soccer/constants/strings.dart';
 
 class ExploreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(AppStrings.EXPLORE),
+    return Scaffold(
+      body: Container(
+        child: ListView.builder(
+          itemBuilder: (context, i) {
+            return ListTile(
+              title:
+                  Text('Roumor Has It: Lampard’s position under threat, ...'),
+              subtitle: Text('04 JAN 2021, 14:06'),
+              trailing: Icon(AppIcons.bookmark),
+            );
+          },
+          itemCount: 10,
+        ),
       ),
     );
   }
