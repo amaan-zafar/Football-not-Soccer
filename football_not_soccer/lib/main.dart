a3543c429260de6740055b5fce4d0421e6b8b35b
Amaan <azafar2000@gmail.com>
2021-06-17 19:53:13 +0530

    flutterfire codes

--- football_not_soccer/lib/main.dart
+++ football_not_soccer/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:football_not_soccer/config/themes/dark_theme.dart';
@@ -7,11 +8,15 @@ import 'package:football_not_soccer/core/leagues/standings/standings_repo.dart';
 import 'package:football_not_soccer/core/root/root_screen.dart';
 import 'package:football_not_soccer/network/api_base_helper.dart';
 import 'package:http/http.dart' as http;
-
 import 'core/leagues/standings/bloc/standings_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Since flutter firebase needs to interact with native platfors, ensureInitialized is required
+  await Firebase.initializeApp();
+
   Bloc.observer = SimpleBlocObserver();
+
   final http.Client _client = http.Client();
   final StandingsRepository _standingsRepository =
       StandingsRepository(apiProvider: ApiProvider(httpClient: _client));
